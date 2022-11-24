d2d71a59ee38254490d7c8205a21a315c2c3b4dc
ikedamakiko0510 <114543112+ikedamakiko0510@users.noreply.github.com>
2022-11-24 15:52:18 +0900

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -6,38 +6,52 @@
   const ul=document.querySelector('ul');
   const slides=ul.children;
   //すべてのスライド＝ulの子要素全ての設定開始
-
+  
   const nav=document.querySelector('nav');
-  const dots=nav.children;
-  dots[0].style.backgroundColor='#999';
+  const dots=[];
+  // dots[0].style.backgroundColor='#999';
   
   let currentindex=0;
-  // function updateDots(){
-  //   dots.forEach(dot => {
-  //     dot.stlye.backgroundColor.remove('#999');
-  //   });
-  //   dots[currentindex].stlye.backgroundColor.add('#999');
-  // }
-  
+   
+
   function moveSlides(){currentindex++;
     const slideWidth=slides[0].getBoundingClientRect().width;
     ul.style.transform=`translateX(${-1*slideWidth*currentindex}px)`;  
-    // updateDots();
-    dots[currentindex].style.backgroundColor='#999';
-    
-    // updateDots(); 丸ボタンの色を変える(currentクラスの移動)
-  }
-     let timer;
-    
+    // dots[currentindex].style.backgroundColor='#999';
+    if (currentindex>3)
+    {console.log(4);
+    reset();       
+    } }
+
+  function reset(){
+    // const slideWidth=slides[0].getBoundingClientRect().width;
+    ul.style.transform=`translateX(240px)`;
+    currentindex=-1;
+    moveSlides();}
+  
+  // function setupDots(){
+  //   for (let i=0;i<slides.length;i++){
+  //   const button=document.createElement('button');
+  //   dots.push(button);
+  //   document.querySelector('nav').appendChild(button);}
+
+  // dots[0].classList.add('current');
+  
+  // function updateDots(){
+  //   dots.forEach(dot=>{dot.classList.remove('current');});
+  //   dots[currentindex].classList.add('current');
+  // }
+  
+    let timer;
+     
      function startSlideshow(){
       timer=setInterval(()=>{moveSlides();
-        if (currentindex===3){clearInterval(timer);
-        }  
-      },5000);
- 
+        },3000);       
+  // if (currentindex===3){clearInterval(timer);いらない？
     }
-
-    startSlideshow();  
+       startSlideshow();  
+      //  setupDots();
+      //  updateDots();
    
 // start.addEventListener('click',()=>{
 //   currentindex=-1;
