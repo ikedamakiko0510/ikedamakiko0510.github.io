dab48bc9a4d33341088db78ed599400ee9763fce
ikedamakiko0510 <114543112+ikedamakiko0510@users.noreply.github.com>
2022-11-25 15:11:46 +0900

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -6,29 +6,48 @@
   const ul=document.querySelector('ul');
   const slides=ul.children;
   //すべてのスライド＝ulの子要素全ての設定開始
-  
-  const nav=document.querySelector('nav');
-  const dots=[];
-  // dots[0].style.backgroundColor='#999';
-  
-  let currentindex=0;
-   
+  // const parent=document.getElementById('parent');
+  // const target1=document.getElementById('child1');
+  // const child1=document.createElement('li');
+  // child1.id="appendChild";
 
-  function moveSlides(){currentindex++;
+  const nav=document.querySelector('nav');
+  const button=document.querySelector('button');
+  const dots=[];
+  dots.push(button);
+  // dots[0].style.backgroundColor='#999';
+
+  let currentindex=0;
+
+   const parent=document.getElementById('parent');
+   const child1=document.getElementById('child1');
+   const child1_copy = child1.cloneNode(true);
+   const child2=document.getElementById('child2');
+   const child2_copy = child2.cloneNode(true); 
+   const child3=document.getElementById('child3');
+   const child3_copy = child3.cloneNode(true); 
+   const child4=document.getElementById('child4');
+   const child4_copy = child4.cloneNode(true); 
+
+   function resetSlides(){
+   parent.appendChild(child1_copy);
+    // child1.remove();
+    parent.appendChild(child2_copy);
+    // child2.remove();
+    parent.appendChild(child3_copy);
+    // child3.remove();
+    parent.appendChild(child4_copy);
+    // child4.remove();
+  }
+   
+  function moveSlides(){
+    currentindex++;
     const slideWidth=slides[0].getBoundingClientRect().width;
     ul.style.transform=`translateX(${-1*slideWidth*currentindex}px)`;  
-    // dots[currentindex].style.backgroundColor='#999';
-    if (currentindex>3)
-    {console.log(4);
-    reset();       
-    } }
+    button.style.backgroundColor='#999';
+    resetSlides();
+     } 
 
-  function reset(){
-    // const slideWidth=slides[0].getBoundingClientRect().width;
-    ul.style.transform=`translateX(240px)`;
-    currentindex=-1;
-    moveSlides();}
-  
   // function setupDots(){
   //   for (let i=0;i<slides.length;i++){
   //   const button=document.createElement('button');
@@ -46,10 +65,12 @@
      
      function startSlideshow(){
       timer=setInterval(()=>{moveSlides();
-        },3000);       
+          },3000); } 
+
   // if (currentindex===3){clearInterval(timer);いらない？
-    }
+    
        startSlideshow();  
+      
       //  setupDots();
       //  updateDots();
    
