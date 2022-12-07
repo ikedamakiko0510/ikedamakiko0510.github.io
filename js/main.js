cac35611a22eab8ccf34d902b30988e37265c90c
ikedamakiko0510 <114543112+ikedamakiko0510@users.noreply.github.com>
2022-12-07 12:27:33 +0900

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -2,24 +2,38 @@
 {   
   const ul=document.querySelector('ul');
   const slides=ul.children;
-  const button=document.querySelector('button');
+  // const button=document.querySelector('button');
   // const current=document.getElemensByClassName('current');
-
   // const btn1=document.getElementById('btn1');
   // const btn2=document.getElementById('btn2');
   // const btn3=document.getElementById('btn3');
   // const btn4=document.getElementById('btn4');
 const dots=[];
-dots.push(button);
- 
+
   let currentindex=0;
+  let n=currentindex%4;
   
+  function updateDots(){
+    dots.forEach(dot => {
+    dot.classList.remove('current')});
+    if(currentindex<4){ 
+    dots[currentindex].classList.add('current');}
+
+    else{
+      let n=currentindex%4;
+      dots[n].classList.add('current');
+      // console.log(n);
+    }
+        
+   }
+   
+
   function moveSlides(){
     currentindex++;
     const slideWidth=slides[0].getBoundingClientRect().width;
     ul.style.transform=`translateX(${-1*slideWidth*currentindex}px)`;  
-    // button.style.backgroundColor='#999';
-     } 
+    // console.log(currentindex);   
+   } 
     
    function resetSlide(){  
      const currentitem1=document.querySelectorAll('li')[0];
@@ -39,23 +53,28 @@ dots.push(button);
      ul.appendChild(copyitem4);
  }
 
- dots[0].classList.add('current');
- function updatedots(){
- dots.forEach(dot => {
-  dot.classList.remove('current');
-    });
-   dots[currentindex].classList.add('current');  
-  }
-
+ function setupDots() {
+   for(let i=0; i<slides.length;i++){
+    const button=document.createElement('button');
+    dots.push(button);
+    document.querySelector('nav').appendChild(button);
+    // nav要素に今作ったボタンを追加
+  }  
+   dots[0].classList.add('current');
+  //  最初のボタンにcurrentクラスをつける
+ }
 
     let timer;
      
      function startSlideshow(){
       timer=setInterval(()=>{moveSlides();
-        resetSlide();updatedots();
-          },3000); } 
+        resetSlide();updateDots();
+          },3000); 
+          } 
     
        startSlideshow();  
+       setupDots();
+       
       
 // window.addEventListener('resize',()=>{
 // moveSlides();
